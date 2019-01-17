890ab49536f95942d46d93939d0b4f2753b0df5d
Forgottenrealms <1456671936@qq.com>
2019-01-17 11:07:40 +0800

    feat: 初始化React项目以及antd配置

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+// import { Carousel, WingBlank } from 'antd-mobile';
+
+class App extends Component {
+  state = {
+    data: ['1', '2', '3'],
+    imgHeight: 176,
+    slideIndex: 2,
+  }
+  
+  render() {
+    return (
+      <div>
+        App
+      </div>
+    );
+  }
+}
+
+export default App;
